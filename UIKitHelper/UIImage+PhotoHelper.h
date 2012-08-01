745088eeb57d89ead7b5defc0553abfc1174171d
achilleus <achilleus@achilleus-no-MacBook.local>
2012-08-01 16:54:49 +0900

    UIImage+PhotoHelperの追加

---
+++ UIKitHelper/UIImage+PhotoHelper.h
@@ -0,0 +1,14 @@
+//
+//  UIImage+PhotoHelper.h
+//  CoreImageFilter
+//
+//  Created by Yasuo Kasajima on 12/07/09.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (PhotoHelper)
+- (UIImage*)cropImageInRect:(CGRect)rect;
+- (UIImage *)resizeImageToWidth:(float )width;
+@end
